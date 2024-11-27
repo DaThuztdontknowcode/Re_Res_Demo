b5997ca09c7e6f37e2ca26dcefac4a69b85b69d8
Duy Thuc Le Dinh <email@domain.com>
2024-11-27 23:31:22 +0700

    ccbmn

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 const config = {
-    API_URL: "https://restaurant-search-api.onrender.com", // Thay URL này bằng URL chính thức khi deploy
+    API_URL: "https://re-res-backend-1.onrender.com", // Thay URL này bằng URL chính thức khi deploy
   };
   
   export default config;
